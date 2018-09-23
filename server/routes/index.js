4faca7640e08f6b2cee9538e1d3b90e0f3daed0f
Alessandro Manfredi <alle.manfredi@gmail.com>
2018-09-23 21:13:19 +0200

    remove comment index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.status(200).json({
         message: "becco frocio"
