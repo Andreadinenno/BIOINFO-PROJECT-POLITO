b38f4fd23f7f08a9cd76c8b2643ccc93951d99ea
Alessandro Manfredi <alle.manfredi@gmail.com>
2018-09-27 12:27:00 +0200

    fix bug routes

--- server/script/script.js
+++ server/script/script.js
@@ -1,12 +1,15 @@
 const exec = require('child_process').exec;
-
+const fs = require('fs');
 
 
 const startScript  = async (options) => {
 
     child = exec('./script/./isomir_sea -h',(error, stdout, stderr)  =>  {
 
-        console.log('stdout: ' + stdout);
+        fs.writeFile('output.txt',stdout, () => {
+            console.log("saved")
+        });
+        
 
         if (error !== null) {
             console.log('exec error: ' + error);
